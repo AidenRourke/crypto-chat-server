385b5edcbef968ad7a9707d7dc31ed071d76a61f
aidenrourke <aidenrourke@gmail.com>
2021-03-14 13:04:17 -0400

    update

--- server/MessagesDAO.js
+++ server/MessagesDAO.js
@@ -1,33 +1,43 @@
 var AWS = require("aws-sdk");
 
 AWS.config.update({
-  region: "us-east-2",
-  endpoint: "http://localhost:4000"
-  // endpoint: "http://Gettingstartedapp-env.eba-sm3mz4hp.us-east-2.elasticbeanstalk.com"
+    region: "us-east-2",
+    endpoint: "https://dynamodb.us-east-2.amazonaws.com",
 });
-var TABLE = "messages";
 
+var docClient = new AWS.DynamoDB.DocumentClient();
 
-async function putMessage(userId, timestamp, receiverName, message) {
-  var client = new AWS.DynamoDB.DocumentClient();
-  var params = {
-      TableName: TABLE,
-      Item: {
-          "user_id": userId,
-          "timestamp": timestamp,
-          "receiver_name": receiverName,
-          "message": message
-      }
-  };
-  client.put(params, function(err, data) {
-      if (err) {
-          console.error("Unable to add item. Error JSON:", JSON.stringify(err));
-      } else {
-          console.log("Added item:", JSON.stringify(data));
-      }
-  });
+console.log(process.env.AWS_PROFILE)
+
+AWS.config.getCredentials(function (err) {
+    if (err) console.log(err.stack);
+    // credentials not loaded
+    else {
+        console.log("Access key:", AWS.config.credentials.accessKeyId);
+        console.log("Region: ", AWS.config.region);
+    }
+});
+
+async function putMessage(userId, timestamp, receiverId, message) {
+    var TABLE = "messages";
+    var params = {
+        TableName: TABLE,
+        Item: {
+            "user_id": userId,
+            "timestamp": `${timestamp}`,
+            "receiver_id": receiverId,
+            "message": message
+        }
+    };
+    docClient.put(params, function (err, data) {
+        if (err) {
+            console.error("Unable to add item. Error JSON:", JSON.stringify(err));
+        } else {
+            console.log("Added item:", JSON.stringify(data));
+        }
+    });
 }
 
 module.exports = {
-  putMessage
-}
+    putMessage
+};
--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const app = require("express")();
 const server = require("http").createServer(app);
 const PORT = process.env.PORT || 4000;
